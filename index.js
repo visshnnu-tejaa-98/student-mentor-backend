6f9ae1cac422888efed1826399d128697015e4e7
visshnnu tejaa <visshnnutejaa@gmail.com>
2021-02-20 09:56:09 +0530

    cors origin added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = 3000;
 app.use(express.json());
 app.use(
 	cors({
-		origin: 'https://suspicious-torvalds-184735.netlify.app/',
+		origin: 'https://visshnnu-student-mentor-frontend.netlify.app',
 	})
 );
 
